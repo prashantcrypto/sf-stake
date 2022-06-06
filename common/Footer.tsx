23fd2e849b539664f27050fc4977b16493460eaa
PatrickWeb3 <PatrickWeb3Services@gmail.com>
2022-06-06 13:12:14 -0500

    Updating new discord links

--- common/Footer.tsx
+++ common/Footer.tsx
@@ -2,7 +2,7 @@ import { darken, lighten } from 'polished'
 import { FaDiscord, FaGithub, FaMedium, FaTwitter } from 'react-icons/fa'
 
 const SOCIALS = {
-  discord: { icon: <FaDiscord />, link: 'https://discord.gg/stonedfarms' },
+  discord: { icon: <FaDiscord />, link: 'https://discord.gg/s7DKM92pt9' },
   twitter: { icon: <FaTwitter />, link: 'https://twitter.com/stonedfarms' },
 }
 
